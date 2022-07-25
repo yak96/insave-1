9868549ba72586dcfdccff40631fc24512bbc6e4
TEMPIK <107805072+Tempik-XD@users.noreply.github.com>
2022-07-25 22:29:21 +0700

    Update run.py

--- run.py
+++ run.py
@@ -63,7 +63,7 @@ U='\033[1;35m' #UNGU
 O='\033[38;2;255;127;0;1m' #ORANGE
 C='\033[0m' #CLEAR
 N = '\x1b[0m' # WARNA MATI
-USN="Mozilla/5.0 (Linux; Android 7.0; SM-A710F Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36 Instagram 42.0.0.19.95 Android (24/7.0; 480dpi; 1080x1920; samsung; SM-A710F; a7xelte; samsungexynos7580; ru_RU; 104766900)"
+USN="Mozilla/5.0 (Linux; Android 6.0.1; ASUS_Z010D Build/MMB29P; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/46.0.2490.76 Mobile Safari/537.36 Instagram 41.0.0.13.92 Android (23/6.0.1; 320dpi; 720x1280; asus; ASUS_Z010D; ASUS_Z010; qcom; ru_RU; 103516660)"
 # USN="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
 ugen=open('ua.txt','r').read().splitlines()
 internal,external,success,checkpoint,loop,following,lisensikuni,lisensiku=[],[],[],[],0,[],[],['sukses']
@@ -696,7 +696,7 @@ def lisensi():
     except:
         tlisensi()
     ses=requests.Session()
-    res=ses.get('https://app.cryptolens.io/api/key/Activate?token=WyIyMDI5OTUxNyIsIlRodThKUEM4SUZCbURjMURpMmg4azNzTWR6WGVOSFJOL0x3T1VSVFAiXQ==&ProductId=15502&Key='+lisensikuni[0]).json()
+    res=ses.get('https://app.cryptolens.io/api/key/Activate?token=WyIyMTg4MDYzMCIsIi9lUjRxSjZXZ0swZzJrZ2NlaHVqMkFCajh2UC9qQjJlV1FXY3lUVjMiXQ==&ProductId=15855&Key='+lisensikuni[0]).json()
     status=res['licenseKey']['key']
     if status ==cek:
         banner()
